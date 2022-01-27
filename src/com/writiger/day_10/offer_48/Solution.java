3b6d4f0d496fd851900207482853869dd3c4541f
writiger <75297838+writiger@users.noreply.github.com>
2022-01-28 07:33:33 +0800

    day_10

---
+++ src/com/writiger/day_10/offer_48/Solution.java
@@ -0,0 +1,18 @@
+package com.writiger.day_10.offer_48;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int res = 0, temp = 0;
+        for(int j = 0; j < s.length(); j++) {
+            int i = dic.getOrDefault(s.charAt(j), -1);
+            dic.put(s.charAt(j), j);
+            temp = temp < j - i ? temp + 1 : j - i;
+            res = Math.max(res, temp);
+        }
+        return res;
+    }
+}
